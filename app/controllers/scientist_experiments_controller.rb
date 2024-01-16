4c8635b0008081e4af8a13412a13571e7c8a8282
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-01-16 11:15:24 -0700

    Add destroy action to ScientistExperiments controller

--- app/controllers/scientist_experiments_controller.rb
+++ app/controllers/scientist_experiments_controller.rb
@@ -4,8 +4,8 @@ class ScientistExperimentsController < ApplicationController
     scientist = Scientist.find(params[:sci_id])
     sci_exp = scientist.find_it(params[:exp_id], scientist.id).first
     sci_exp.destroy
+
     redirect_to "/scientists/#{params[:sci_id]}"
   end
-
 
 end
