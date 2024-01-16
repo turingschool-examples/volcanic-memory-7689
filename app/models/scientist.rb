5702ce88723099232796bb17e0b369ad2e6a1f46
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-01-16 11:53:44 -0700

    fix: Remove #all_experiments method from Scientist and spec

--- app/models/scientist.rb
+++ app/models/scientist.rb
@@ -2,8 +2,4 @@ class Scientist < ApplicationRecord
   belongs_to :lab
   has_many :scientist_experiments
   has_many :experiments, through: :scientist_experiments
-
-  def all_experiments
-    Experiment.select("string_agg(experiments.name, ', ')").joins(:scientists).where("scientists.id = ?", self.id).pluck(:name).join(", ")
-  end
 end
--- spec/models/scientist_spec.rb
+++ spec/models/scientist_spec.rb
@@ -19,14 +19,4 @@ RSpec.describe Scientist do
     @scientist_experiments_1 = ScientistExperiment.create!(scientist_id: @scientist_1.id, experiment_id: @experiment_1.id)
     @scientist_experiments_2 = ScientistExperiment.create!(scientist_id: @scientist_2.id, experiment_id: @experiment_2.id)
   end
-
-  describe "instance methods" do
-    describe "#all_experiments" do
-      it "lists all of the experiments for a scientist" do
-        new_experiment = ScientistExperiment.create!(scientist_id: @scientist_1.id, experiment_id: @experiment_2.id)
-
-        expect(@scientist_1.all_experiments).to eq("MINERvA, Time Pod")
-      end
-    end
-  end
 end
