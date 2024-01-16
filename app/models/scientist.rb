8830793b58d2a2656b8365b2ab75c1198f9a6e32
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-01-16 11:57:15 -0700

    Add #count_exp to scientist model

--- app/models/scientist.rb
+++ app/models/scientist.rb
@@ -6,4 +6,8 @@ class Scientist < ApplicationRecord
   def find_it(exp_id, sci_id)
     ScientistExperiment.where("experiment_id = #{exp_id}").where("scientist_id = #{sci_id}")
   end
+
+  def count_exp
+    experiments.count
+  end
 end
