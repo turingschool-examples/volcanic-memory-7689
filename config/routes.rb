50a2981f00dd8a5950937730875dfb79ac98d5f4
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-01-16 10:43:06 -0700

    Add routes to scientist and old style delete for scient_exps

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+
+  resources :scientists, only: [:show, :destroy] do
+    resources :scientist_experiments, controller: 'scientist_experiments'
+  end
+
+  delete "/scientist_experiments", to: "scientist_experiments#destroy"
 end
