842e4b6ae0c4fabb755b89c5f005cdc3a69f0fd2
Barry Allahyar <barrya@gmail.com>
2024-01-16 12:59:06 -0600

    Update scientist controller to include lab as well as experiments

--- app/controllers/scientists_controller.rb
+++ app/controllers/scientists_controller.rb
@@ -1,5 +1,7 @@
 class ScientistsController < ApplicationController
   def show
     @scientist = Scientist.find(params[:id])
+    @lab = @scientist.lab
+    @experiments = @scientist.experiments
   end
 end
