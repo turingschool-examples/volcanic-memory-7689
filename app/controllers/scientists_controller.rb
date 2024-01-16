7a6344354680897226a927b7c3ddef701e83e00b
Barry Allahyar <barrya@gmail.com>
2024-01-16 13:55:30 -0600

    In the middle but submitting. Started late and got too hung up on random thnigns as usual >.<  Will have to do the redo.

--- app/controllers/scientists_controller.rb
+++ app/controllers/scientists_controller.rb
@@ -1,7 +1,5 @@
 class ScientistsController < ApplicationController
   def show
     @scientist = Scientist.find(params[:id])
-    @lab = @scientist.lab
-    @experiments = @scientist.experiments
   end
 end
--- spec/features/experiments/index_spec.rb
+++ spec/features/experiments/index_spec.rb
@@ -2,18 +2,24 @@ require "rails_helper"
 
 describe "Experiment Index page" do
   before :each do
-    @lab1 = Lab.create!(name: "CERN")
-    @lab2 = Lab.create!(name: "SLAC")
-    @scientist_1 = Scientist.create!(name: "Planck", specialty: "Physics", university: "University of Munich", lab_id: @lab1.id)
-    @scientist_2 = Scientist.create!(name: "Newton", specialty: "Physics", university: "Cambridge", lab_id: @lab1.id)
-    @scientist_3 = Scientist.create!(name: "Einstein", specialty: "Physics", university: "University of Zurich", lab_id: @lab2.id)
-    @scientist_4 = Scientist.create!(name: "Bohr", specialty: "Physics", university: "	University of Copenhagen", lab_id: @lab2.id)
+    @lab_1 = Lab.create!(name: "CERN")
+    @lab_2 = Lab.create!(name: "SLAC")
+    @scientist_1 = @lab_1.scientsts.create!(name: "Planck", specialty: "Physics", university: "University of Munich")
+    @scientist_2 = @lab_1.scientsts.create!(name: "Newton", specialty: "Physics", university: "Cambridge")
+    @scientist_3 = @lab_2.scientsts.create!(name: "Einstein", specialty: "Physics", university: "University of Zurich")
+    @scientist_4 = @lab_2.scientsts.create!(name: "Bohr", specialty: "Physics", university: "	University of Copenhagen")
     @experiment_1 = Experiment.create!(name: "Experiment 1", objective: "Theoretical Physics", num_months: 4)
     @experiment_2 = Experiment.create!(name: "Experiment 2", objective: "Theoretical Physics", num_months: 10)
     @experiment_3 = Experiment.create!(name: "Experiment 3", objective: "Theoretical Physics", num_months: 15)
     @experiment_4 = Experiment.create!(name: "Experiment 4", objective: "Quantum Mechanics", num_months: 12)
     @experiment_5 = Experiment.create!(name: "Experiment 5", objective: "Quantum Mechanics", num_months: 24)
     @experiment_6 = Experiment.create!(name: "Experiment 6", objective: "Quantum Mechanics", num_months: 48)
+
+    @science_experiment_1 = ScientistExperiment.create!(scientist: @scientist_1, experiment: @experiment_1)
+    @science_experiment_4 = ScientistExperiment.create!(scientist: @scientist_1, experiment: @experiment_2)
+    @science_experiment_2 = ScientistExperiment.create!(scientist: @scientist_2, experiment: @experiment_1)
+    @science_experiment_3 = ScientistExperiment.create!(scientist: @scientist_3, experiment: @experiment_2)
+    @science_experiment_4 = ScientistExperiment.create!(scientist: @scientist_4, experiment: @experiment_4)
   end
 
 end
