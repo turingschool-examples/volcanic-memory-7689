ed071db76a75b79e4038c419b77e4f4c78f0e39e
Nash <141100397+s2an@users.noreply.github.com>
2024-01-16 12:58:26 -0500

    Create initial test for User Story 2

--- spec/features/scientists/show_spec.rb
+++ spec/features/scientists/show_spec.rb
@@ -33,4 +33,37 @@ RSpec.describe "the scientist show page", type: :feature do
       expect(page).to have_content(experiment_2.name)
     end
   end
+
+  describe "User Story 2, Remove an Experiment from a Scientist" do
+    # As a visitor
+    # When I visit a scientist's show page
+    # Then next to each experiment's name, I see a button to remove that experiment from that scientist's work load
+    # When I click that button for one experiment
+    # I'm brought back to the scientist's show page
+    # And I no longer see that experiment's name listed
+    # And when I visit a different scientist's show page that is working on that same experiment,
+    # Then I see that the experiment is still on the other scientist's work load"
+
+    it "adds a button to remove an experiment and confirms it works" do
+      lab = Lab.create!(name: "Fermilab")
+      scientist_1 = lab.scientists.create!(name: "Marie Curie", specialty: "radioactivity", university: "University of Paris")
+      scientist_2 = lab.scientists.create!(name: "Jeff Daniels", specialty: "data", university: "Community College")
+      experiment = Experiment.create!(name: "Abby Normal", objective: "Assemble and animate a collection of corpses", num_months: 12)
+      
+      scientist_1.experiments << experiment
+      scientist_2.experiments << experiment
+
+      visit scientist_path(scientist_1)
+      
+      expect(page).to have_button("Remove")
+      
+      click_button("Remove")
+      
+      expect(current_path).to eq(scientist_path(scientist_1))
+      expect(page).to_not have_contents(experiment.name)
+      
+      visit scientist_path(scientist_2)
+      
+      expect(page).to have_contents(experiment.name)
+    end
 end
