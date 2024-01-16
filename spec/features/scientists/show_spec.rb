624e777e8d61f7d6d88d47e70d9f3348ea0c2060
Meg Roth <146018731+megroth2@users.noreply.github.com>
2024-01-16 09:53:31 -0700

    complete user story 1

--- spec/features/scientists/show_spec.rb
+++ spec/features/scientists/show_spec.rb
@@ -4,6 +4,12 @@ RSpec.describe "the scientist show page" do
   before(:each) do
     @lab_1 = Lab.create!(name: "Fermilab")
     @scientist_1 = Scientist.create!(name: "Marie Curie", specialty: "radioactivity", university: "University of Paris", lab: @lab_1)
+    @experiment_1 = Experiment.create!(name: "MINERVA", objective: "study neutrino reactions with five different nuclei", num_months: 4)
+    @experiment_2 = Experiment.create!(name: "water test", objective: "study water molecules", num_months: 2)
+    @experiment_3 = Experiment.create!(name: "fire test", objective: "study fire", num_months: 8)
+
+    @scientist_experiment_1 = ScientistExperiment.create!(scientist_id: @scientist_1.id, experiment_id: @experiment_1.id)
+    @scientist_experiment_2 = ScientistExperiment.create!(scientist_id: @scientist_1.id, experiment_id: @experiment_2.id)
 
     visit scientist_path(@scientist_1)
   end
@@ -23,7 +29,10 @@ RSpec.describe "the scientist show page" do
       expect(page).to have_content("Specialty: #{@scientist_1.specialty}")
       expect(page).to have_content("University: #{@scientist_1.university}")
       expect(page).to have_content("Lab: #{@scientist_1.lab.name}")
-      expect(page).to have_content("Lab: #{@scientist_1.experiments}") # does this need to be name?
+      expect(page).to have_content("#{@experiment_1.name}")
+      expect(page).to have_content("#{@experiment_2.name}")
+      expect(page).to_not have_content("#{@experiment_3.name}")
+
     end
   end
 end
