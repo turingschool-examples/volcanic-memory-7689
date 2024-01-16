b5400bfdb970b8af5525f9208fbfbb7506fbed99
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-01-16 10:42:41 -0700

    Add scientist model with find it method

--- app/models/scientist.rb
+++ app/models/scientist.rb
@@ -1,3 +1,9 @@
 class Scientist < ApplicationRecord
   belongs_to :lab
+  has_many :scientist_experiments
+  has_many :experiments, through: :scientist_experiments
+
+  def find_it(exp_id, sci_id)
+    ScientistExperiment.where("experiment_id = #{exp_id}").where("scientist_id = #{sci_id}")
+  end
 end
