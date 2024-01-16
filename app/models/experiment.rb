3d25956da1e4283e2bf6fcaf2999308fe458cf00
Barry Allahyar <barrya@gmail.com>
2024-01-16 11:53:54 -0600

    Fix model associations for experiment.rb

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -1,4 +1,4 @@
 class Experiment < ApplicationRecord
-  has_many :experiments_scientists, through: :scientists
-  has_many :experiments, through: :experiments_scientists
+  has_many :experiments_scientists
+  has_many :scientists, through: :experiments_scientists
 end
